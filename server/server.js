9e4418dee62818cfb4f53742323856f29493ae79
JavkhlanGanbat <javkhlanganbat2003@gmail.com>
2025-01-03 11:16:12 +0800

    .

--- server/server.js
+++ server/server.js
@@ -25,5 +25,5 @@ app.get('*', (req, res) => {
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
-    console.log(`Serving static files from: ${projectRoot}`);
+    console.log(`http://localhost:${PORT}`);
 });
